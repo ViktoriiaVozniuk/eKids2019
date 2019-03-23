d5f43f32786b763de9ac63b8b66acc70f0991270
vika <victoria.v.ua@gmail.com>
2019-03-23 13:28:10 +0200

    heart

--- diamond.py
+++ diamond.py
@@ -8,13 +8,13 @@ def draw_shape(sides, length):
         #fillcolor("green")
     end_fill()
 
-def draw_matrix(matrix):
+def draw_mat_rix(mat_rix):
     startX = -200
     startY = 200
     shapeSide = 50
-    for row in range (len (matrix)):
-        for col in range (len(matrix[row])):
-            cell = matrix [row][col]
+    for row in range (len (mat_rix)):
+        for col in range (len(mat_rix[row])):
+            cell = mat_rix [row][col]
             if cell == 1 :
                 fillcolor ("red")
             else :
@@ -23,7 +23,7 @@ def draw_matrix(matrix):
             curY = startY - shapeSide * row
             goto (curX, curY)
             draw_shape(4,shapeSide )
-matrix = [
+mat_rix = [
           [0, 1, 1, 0, 0, 1, 1, 0],
           [1, 1, 1, 0, 0, 1, 1, 1],
           [1, 1, 1, 1, 1, 1, 1, 1],
@@ -38,8 +38,7 @@ setup (width=600, height=600, startx=0, starty=0)
 color('yellow')
 # shape("turtle")
 hideturtle()
-
 speed(30)
-draw_matrix(matrix)
+draw_mat_rix(mat_rix)
 
 exitonclick()
